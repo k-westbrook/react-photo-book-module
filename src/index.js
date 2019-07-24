6afcf5cbbda2c5441569c8ae6674105ec84a9b82
k-westbrook <kcheriewestbrook@gmail.com>
2019-07-23 19:04:33 -0700

    updated css

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import './style.css';
+
+export default class Row extends Component {
+
+
+  render() {
+    let counter = -1;
+    return (
+      <div className="container">
+        {this.props.data.map(element => {
+          counter += 1;
+          return (
+            <div
+              key={counter}
+              className="element-block">
+              <div className="image-container">
+                <img src={element.imageURL} className="image" />
+              </div>
+              <div className="text-container">
+                <h3>{element.name}</h3>
+              </div>
+            </div>
+          )
+        })
+        }
+      </div>
+    );
+  }
+}
